7cb48874498a84000d44226f680150296bde9ca6
ElijahCoding <cuiyeqing960904@gmail.com>
2020-01-05 15:38:56 +0300

    handling voice data response

--- Practical Experience/Alexis Speech Assistant.py
+++ Practical Experience/Alexis Speech Assistant.py
@@ -12,8 +12,21 @@ import os # to remove created audio files
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print('SAY SOMETHING')
-    audio = r.listen(source)
-    voice_data = r.recognize_google(audio)
-    print(voice_data)
+def record_audio():
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        try:
+            voice_data = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            print('Sorry, I did not get that....')
+        except sr.RequestError:
+            print('Sorry, my speech service is down')
+        return voice_data
+
+def respond(voice_data):
+    pass
+
+print('How can I help you?')
+voice_data = record_audio()
+print(voice_data)
+respond(voice_data)
